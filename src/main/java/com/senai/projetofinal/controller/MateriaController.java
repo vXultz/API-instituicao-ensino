d000deab817cf7af8619a4016b394e16299266c1
Vitor Schultz <vitorserta@gmail.com>
2024-04-16 14:54:23 -0300

    adicionar get por id e delete por id de materias

--- src/main/java/com/senai/projetofinal/controller/MateriaController.java
+++ src/main/java/com/senai/projetofinal/controller/MateriaController.java
@@ -1,12 +1,13 @@
 package com.senai.projetofinal.controller;
 
+import com.senai.projetofinal.controller.dto.request.materia.InserirMateriaRequest;
+import com.senai.projetofinal.controller.dto.response.materia.MateriaResponse;
 import com.senai.projetofinal.datasource.entity.MateriaEntity;
+import com.senai.projetofinal.service.CursoService;
 import com.senai.projetofinal.service.MateriaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class MateriaController {
 
     private final MateriaService service;
 
-    public MateriaController(MateriaService service) {
+    public MateriaController(MateriaService service, CursoService cursoService) {
         this.service = service;
     }
 
@@ -25,5 +26,33 @@ public class MateriaController {
             @RequestHeader("Authorization") String token) {
         List<MateriaEntity> listarMaterias = service.listarTodos(token.substring(7));
         return ResponseEntity.ok().body(listarMaterias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MateriaEntity> buscarMateriaPorId(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String token) {
+        MateriaEntity materia = service.buscarPorId(id, token.substring(7));
+        return ResponseEntity.ok(materia);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criarMateria(
+            @RequestBody InserirMateriaRequest inserirMateriaRequest,
+            @RequestHeader("Authorization") String token) {
+        try {
+            MateriaResponse criarMateriaResponse = service.salvar(inserirMateriaRequest, token.substring(7));
+            return new ResponseEntity<>(criarMateriaResponse, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarMateria(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String token) {
+        service.removerPorId(id, token.substring(7));
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/senai/projetofinal/service/MateriaService.java
+++ src/main/java/com/senai/projetofinal/service/MateriaService.java
@@ -1,5 +1,8 @@
 package com.senai.projetofinal.service;
 
+import com.senai.projetofinal.controller.dto.request.materia.InserirMateriaRequest;
+import com.senai.projetofinal.controller.dto.response.materia.MateriaResponse;
+import com.senai.projetofinal.datasource.entity.CursoEntity;
 import com.senai.projetofinal.datasource.entity.MateriaEntity;
 import com.senai.projetofinal.datasource.repository.CursoRepository;
 import com.senai.projetofinal.datasource.repository.MateriaRepository;
@@ -8,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -41,5 +45,59 @@ public class MateriaService {
         log.info("Todas as matérias listadas");
 
         return repository.findAll();
+    }
+
+    public MateriaEntity buscarPorId(Long id, String token) {
+        String role = tokenService.buscaCampo(token, "scope");
+
+        if (!"admin".equals(role) && !"pedagogico".equals(role)) {
+            throw new SecurityException("Usuário não autorizado");
+        }
+
+        log.info("Matéria com id {} encontrada", id);
+        return repository.findById(id).orElseThrow(() -> new NotFoundException("Matéria não encontrada"));
+    }
+
+    public MateriaResponse salvar(InserirMateriaRequest inserirMateriaRequest, String token) {
+        String role = tokenService.buscaCampo(token, "scope");
+
+        if (!"admin".equals(role) && !"pedagogico".equals(role)) {
+            throw new SecurityException("Usuário não autorizado");
+        }
+
+        if (inserirMateriaRequest.nome() == null || inserirMateriaRequest.nome().isBlank()) {
+            throw new IllegalArgumentException("Nome não pode ser nulo ou vazio");
+        }
+
+        CursoEntity curso = cursoRepository.findById(inserirMateriaRequest.curso())
+                .orElseThrow(() -> new NotFoundException("Curso não encontrado"));
+
+        MateriaEntity materia = new MateriaEntity();
+        materia.setNome(inserirMateriaRequest.nome());
+        materia.setCurso(curso);
+
+        MateriaEntity materiaSalva = repository.save(materia);
+
+        log.info("Salvando matéria com o nome {}", inserirMateriaRequest.nome());
+
+        return new MateriaResponse(
+                materiaSalva.getId(),
+                materiaSalva.getNome(),
+                materiaSalva.getCurso());
+    }
+
+    public void removerPorId(Long id, String token) {
+        String role = tokenService.buscaCampo(token, "scope");
+
+        if (!"admin".equals(role) && !"pedagogico".equals(role)) {
+            throw new SecurityException("Usuário não autorizado");
+        }
+
+        if (!repository.existsById(id)) {
+            throw new NotFoundException("Nenhuma matéria encontrada com o id passado");
+        }
+
+        log.info("Removendo matéria com o id {}", id);
+        repository.deleteById(id);
     }
 }
