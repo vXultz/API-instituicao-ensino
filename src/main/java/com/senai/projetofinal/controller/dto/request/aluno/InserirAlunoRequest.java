aee938a567c1ff0fce81fd67e9fcb84e6e0e7bfd
Vitor Schultz <vitorserta@gmail.com>
2024-04-19 13:36:34 -0300

    ajustar a entidade aluno para receber usuario_id, aluno só pode ser salvo se o usuario tiver papel aluno

--- src/main/java/com/senai/projetofinal/controller/dto/request/aluno/InserirAlunoRequest.java
+++ src/main/java/com/senai/projetofinal/controller/dto/request/aluno/InserirAlunoRequest.java
@@ -3,6 +3,7 @@ package com.senai.projetofinal.controller.dto.request.aluno;
 public record InserirAlunoRequest(
         String nome,
         String dataNascimento,
+        Long usuario,
         Long turma
 ) {
 }
